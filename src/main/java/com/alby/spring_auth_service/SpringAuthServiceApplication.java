25504e38334f6f595968cae6edcfab600e6006c7
Alby Al Mugni <mugnialby@gmail.com>
2024-11-13 16:04:49 +0700

    Excluded spring security auto configuration

--- src/main/java/com/alby/spring_auth_service/SpringAuthServiceApplication.java
+++ src/main/java/com/alby/spring_auth_service/SpringAuthServiceApplication.java
@@ -1,9 +1,13 @@
 package com.alby.spring_auth_service;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {
+		SecurityAutoConfiguration.class})
 public class SpringAuthServiceApplication {
 
 	public static void main(String[] args) {
